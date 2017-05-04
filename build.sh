495e5a6b53099abe46b85400b08bc43d2ae993ed
Silu Huang <annasilu0523@gmail.com>
2017-05-04 12:40:33 -0500

    add

--- build.sh
+++ build.sh
@@ -6,11 +6,12 @@ rm -rf *.pdf
 rm -rf *.log
 #rm body.tex
 echo "preparing latex"
-#python gdoc2latex.py "https://docs.google.com/document/d/1250KVkX4lfCBlHjB35O-q0AnfDKTLMKgJei_goj5fNU/edit"  > body.tex 
+python gdoc2latex.py "https://docs.google.com/document/d/1dzCMpYgPO8FswOI56ynHMB19043unNG43ngKhJokmZE/edit?usp=sharing"  > separability.tex 
+python gdoc2latex.py "https://docs.google.com/document/d/1GkEQ2LS4-E6yCdB0tgV-fzUjdUBzeOZ1ffe7aiYWzUU/edit?usp=sharing"  > body.tex 
 echo "latexing"
 pdflatex separability.tex
 echo "creating bibtex"
-python gdoc2latex.py "https://docs.google.com/document/d/1LpVyucw8MNplvaLsRehRBzmKMgfCC2TI8pNOv9E9YWk/edit" > ref.bib
+python gdoc2latex.py "https://docs.google.com/document/d/1KLq-8gsntsIpZA5LIHDXUB2sR849G8oqWaGuVYF78u4/edit?usp=sharing" > ref.bib
 bibtex separability
 echo "creating PDF"
 pdflatex separability.tex
@@ -23,4 +24,5 @@ rm -rf *.log
 rm -rf *.out
 #rm body.tex
 #rm ref.bib
+#rm separability.tex
 echo "DONE"
