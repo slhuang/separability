e34f7c8a0dbe50d4c3be59b0ae45fb4dd5e42068
Silu Huang <annasilu0523@gmail.com>
2018-04-16 10:31:47 -0500

    update

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+rm -rf *.aux
+rm -rf *.bbl
+rm -rf *.blg
+rm -rf *.pdf
+rm -rf *.log
+echo "preparing latex"
+TAG=`date +'%y%m%d'`
+python gdoc2latex.py "https://docs.google.com/document/d/1dzCMpYgPO8FswOI56ynHMB19043unNG43ngKhJokmZE/edit?usp=sharing"  > separability.tex
+python gdoc2latex.py "https://docs.google.com/document/d/1GkEQ2LS4-E6yCdB0tgV-fzUjdUBzeOZ1ffe7aiYWzUU/edit?usp=sharing"  > body.tex
+echo "latexing"
+pdflatex separability.tex
+echo "creating bibtex"
+python gdoc2latex.py "https://docs.google.com/document/d/1KLq-8gsntsIpZA5LIHDXUB2sR849G8oqWaGuVYF78u4/edit?usp=sharing" > ref.bib
+bibtex separability
+echo "creating PDF"
+pdflatex separability.tex
+
+rm -rf *.aux
+rm -rf *.bbl
+rm -rf *.blg
+rm -rf *.log
+rm -rf *.out
+echo "DONE"
