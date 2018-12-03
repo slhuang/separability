f80bbac3ed9e3d578727f0e1dd23b92d5c6fe7b1
Silu Huang <annasilu0523@gmail.com>
2018-12-03 12:02:10 -0600

    eps

--- fig/pdf2eps.sh
+++ fig/pdf2eps.sh
@@ -4,6 +4,6 @@
 # usage:
 # pdf2eps <page number> <pdf file without ext>
 
-pdfcrop "$2.pdf" "$2-temp.pdf"
-pdftops -f $1 -l $1 -eps "$2-temp.pdf" "$2.eps"
-rm  "$2-temp.pdf"
+#pdfcrop "$2.pdf" "$2-temp.pdf"
+pdftops -f $1 -l $1 -eps "$2.pdf" "$2.eps"
+#rm  "$2-temp.pdf"
